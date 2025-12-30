034a935b95c24fb05404515b1575204565881cc2
sumedhac802 <sumedhac802@gmail.com>
2025-12-30 12:44:34 -0600

    Your descriptive commit message

---
+++ fall2025/ITSC_1213/proj1/Dog.java
@@ -0,0 +1,36 @@
+package fall2025.ITSC_1213.proj1;
+
+public class Dog {
+
+    private String name;
+    private String breed;
+
+    public Dog(String name, String breed)
+    {
+        this.name = name;
+        this.breed = breed;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setBreed(String breed)
+    {
+        this.breed = breed;
+    }
+    public String getBreed()
+    {
+        return this.breed;
+    }
+
+    public void play(Dog d2)
+    {
+        System.out.println(getName() + " is playing with " + d2.getName());
+    }
+}
---
+++ fall2025/ITSC_1213/proj1/DogPark.java
@@ -0,0 +1,32 @@
+package fall2025.ITSC_1213.proj1;
+
+import java.util.Scanner;
+
+public class DogPark {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name of d1: ");
+        String nameD1 = scanner.nextLine();
+
+        System.out.println("Breed of d1: ");
+        String breedD1 = scanner.nextLine();
+
+        System.out.println("Name of d2: ");
+        String nameD2 = scanner.nextLine();
+
+        System.out.println("Breed of d2: ");
+        String breedD2 = scanner.nextLine();
+
+        scanner.close();
+
+        Dog d1 = new Dog(nameD1, breedD1);
+        Dog d2 = new Dog(nameD2, breedD2);
+
+        d1.play(d2);
+
+    }
+
+}
